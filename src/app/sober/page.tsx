e2ced92bf3a68be007e7903445f97bd32650dcdd
shadow-slave <ashwinsunil02@gmail.com>
2026-02-06 21:43:39 +0530

    updated sober page

--- src/app/sober/page.tsx
+++ src/app/sober/page.tsx
@@ -7,40 +7,62 @@ import { Playfair_Display,Lato } from "next/font/google";
 const playfair = Playfair_Display({ subsets: ["latin"] });
 const lato = Lato({
     subsets: ["latin"],
-    weight: "100"
+    weight: "400"
 });
+
+
+const getNext7Days = () =>{
+    const days = []
+    for(let i=0;i<7;i++){
+        const date = new Date()
+        date.setDate(date.getDate() + i)
+        const formattedDate = date.toLocaleDateString("en-IN",{month:"long", day:"numeric"})
+        days.push(formattedDate)
+    }
+        // console.log(days)
+
+    return days
+}
 
 export default function SoberPage(){
     const todayIndex = new Date().getDay(); 
     const [currentIndex, setCurrentIndex] = useState(todayIndex);
     const currentDrink = soberDrinks[currentIndex];
+    const [currentDate, setDate] = useState<string[]>([]);
 
-    const isToday = currentIndex === todayIndex;
-
+    // console.log(todayIndex)
+    // console.log(currentIndex)
     useEffect(()=>{
+        setDate(getNext7Days())
         const i = setInterval(()=>{
             setCurrentIndex((prevIndex)=>(prevIndex === 6 ? 0 : prevIndex + 1))
         }, 3000)
         return () => clearInterval(i);
     }, [])
+    const isToday = currentIndex === todayIndex;
+    const dayoffset = (currentIndex - todayIndex + 7) % 7;
+    const displayDate = currentDate[dayoffset] || "Loading..."
 
     return(
-  <div className="main min-h-screen flex flex-col md:flex-row ">
-             <button className="flex text-slate-400 absolute top-1/2 left-3 text-xs md:text-2xl md:hover:cursor-pointer " onClick={()=>setCurrentIndex((prev)=>prev<6?prev+1:6)}>
-                  →
-             </button>
+<div className="main min-h-screen flex flex-col md:flex-row ">
+            <button  className="hidden md:flex text-stone-300 absolute top-1/2 left-3 text-lg z-50 md:hover:cursor-pointer hover:text-zinc-800  transition-all animate-slide-in-left duration-300 hover:translate-x-2" 
+                onClick={() => setCurrentIndex((prev) => prev < 6 ? prev + 1 : 0)}>
+                →
+            </button>
              {/* <button className="flex text-sky-700 absolute top-1/2 right-3 text-xs md:text-2xl md:hover:cursor-pointer " onClick={()=>setCurrentIndex((prev)=>prev<6?prev+1:0)}>
                 
              </button> */}
              <div className="left bg-[#f8f5f2] flex items-center justify-center w-full md:w-1/2  p-10 relative z-10">
                  <div key={currentDrink.name} className={`${lato.className} text-center flex flex-col items-center justify-center animate-fade-up`}>
-                     {isToday && <h2 
+                            {isToday &&
+                                <h2 
                                      className="mt-8 px-4 py-1 mb-2 text-white uppercase tracking-widest text-xs"
                                      style={{backgroundColor:currentDrink.color}}
                                  >
                                      Today's Choice
-                                 </h2>}
-                     <h1 className={`${lato.className} text-sm md:text-base tracking-[0.2em] uppercase mb-2 text-zinc-800 `}>{currentDrink.day} | <span> {currentDate} </span></h1>
+                                 </h2>
+                            }
+                     <h1 className={`${lato.className} text-sm md:text-base tracking-[0.2em] uppercase mb-2 text-zinc-800 `}>{currentDrink.day} | <span> {displayDate} </span></h1>
                      <h1 className={`${playfair.className} text-5xl md:text-7xl mb-4 text-zinc-800 `}>{currentDrink.name}</h1>
                      <div className="flex gap-2 mb-6">
                         {currentDrink.tags.map((t)=>(
@@ -59,5 +81,7 @@ export default function SoberPage(){
              >
                  <Image  src={currentDrink.image || ""} alt={currentDrink.name} fill priority sizes="(max-width: 768px) 100vw, 50vw" className="object-contain"/>
              </div>
-         </div>    )
+         </div>
+
+           )
 }
